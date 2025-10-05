1a5ef274addd2fad72621b1b51751712a8a537ec
kindabrazy <kindofbrazy@gmail.com>
2025-10-05 18:34:38 +0330

    fix(renderer): correct button label in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="absolute inset-0 bg-[#212121] flex items-center justify-center">
       <ButtonGroup>
         <Button color="primary">Button 1</Button>
-        <Button color="secondary">Button 1</Button>
+        <Button color="secondary">Button 2</Button>
       </ButtonGroup>
     </div>
   );
