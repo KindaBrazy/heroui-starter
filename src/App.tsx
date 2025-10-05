f8638897309731e7379c7fe2f8ce23c8ec2a76ef
kindabrazy <kindofbrazy@gmail.com>
2025-10-05 18:32:20 +0330

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {Button, ButtonGroup} from '@heroui/react';
+
+function App() {
+  return (
+    <div className="absolute inset-0 bg-[#212121] flex items-center justify-center">
+      <ButtonGroup>
+        <Button color="primary">Button 1</Button>
+        <Button color="secondary">Button 1</Button>
+      </ButtonGroup>
+    </div>
+  );
+}
+
+export default App;
